16002c40d283bd50a76efe6950a1539575785560
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-06-25 00:53:04 +0200

    UI/UX

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -5,15 +5,11 @@ const FooterComponent = () => {
     return (
 
         <Row style={{ textAlign: "center", backgroundColor: "#77777F", height: "140px" }}>
-            <Col className="mt-3">
-                HFH
+
+            <Col className="mt-4">
+                <Button size="md" style={{ backgroundColor: "#262020", borderRadius: "50%", fontSize: "1.3rem" }} onClick={() => window.scrollTo(0, 0)} >^</Button>
             </Col>
-            <Col className="mt-3">
-                <Button size="md" style={{ backgroundColor: "#262020", borderRadius: "50%" }} onClick={() => window.scrollTo(0, 0)} >^</Button>
-            </Col>
-            <Col className="mt-3">
-                Refugeeks
-            </Col>
+
         </Row>
 
     );
