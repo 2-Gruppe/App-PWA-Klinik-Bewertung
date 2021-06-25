ed032ff28493a787c49d610f49d2b36ce3aaecdf
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-06-26 01:08:29 +0200

    Dark Mode addiert

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -5,11 +5,17 @@ const FooterComponent = () => {
     return (
 
         <Row style={{ textAlign: "center", backgroundColor: "#77777F", height: "140px" }}>
-
-            <Col className="mt-4">
-                <Button size="md" style={{ backgroundColor: "#262020", borderRadius: "50%", fontSize: "1.3rem" }} onClick={() => window.scrollTo(0, 0)} >^</Button>
+            <Col className="mt-3">
+                <br />
+                <p className="text-white">H F H</p>
             </Col>
-
+            <Col className="mt-3">
+                <Button size="md" style={{ backgroundColor: "#262020", borderRadius: "50%" }} onClick={() => window.scrollTo(0, 0)} >^</Button>
+            </Col>
+            <Col className="mt-3">
+                <br />
+                <p className="text-white">R e f u g e e k s</p>
+            </Col>
         </Row>
 
     );
